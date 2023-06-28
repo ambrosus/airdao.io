d9d6d45247a7383034b9a13c42273b5cd43cc54f
d.voronin <echelon1109@gmail.com>
2023-06-28 14:21:33 +0300

    added data to stats

--- src/components/Homepage/Trade/Stats.js
+++ src/components/Homepage/Trade/Stats.js
@@ -4,18 +4,48 @@ import styles from './trade.module.scss';
 import logo from './logo.png';
 import Image from 'next/image';
 import graphMock from './graph-mock.png';
+import {useEffect, useState} from "react";
+
+function numberWithCommas(x) {
+  return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 export default function Stats() {
+  const [ambInfo, setAmbInfo] = useState({});
+
+  useEffect(() => {
+    getStats();
+  }, []);
+
+  const getStats = async () => {
+    const response = await fetch('https://token.ambrosus.io/');
+    const json = await response.json();
+    console.log(json);
+    setAmbInfo(json.data);
+  };
+
   return (
     <div className={styles.stats_container}>
       <div className={styles.price_chart}>
         <div className={styles.chart_top}>
           <Image src={logo} alt={'logo'} className={styles.chart_logo} />
           <div className={styles.chart_top_left}>AirDAO</div>
-          <div className={styles.chart_top_right}>$0.0130</div>
+          <div className={styles.chart_top_right}>
+            ${ambInfo.price_usd.toFixed(3)}
+          </div>
           <div className={styles.chart_bottom_left}>AMB</div>
-          <div className={styles.chart_bottom_right}>
-            +20.17% <span className={styles.gray}>(24hrs)</span>
+          <div
+            className={styles.chart_bottom_right}
+            style={
+              ambInfo.percent_change_24h < 0
+                ? { color: 'rgb(254, 99, 109)' }
+                : {}
+            }
+          >
+            {`${ambInfo.percent_change_24h > 0 ? '+' : ''}${
+              ambInfo.percent_change_24h
+            } `}
+            <span className={styles.gray}>(24hrs)</span>
           </div>
         </div>
         <Image src={graphMock} alt={'graph'} className={styles.chart_graph} />
@@ -30,17 +60,23 @@ export default function Stats() {
         className={`${styles.floating_container} ${styles.circulating_supply}`}
       >
         <div className={styles.floating_name}>Circulating Supply</div>
-        <div className={styles.floating_value}>1,543,750,660 AMB</div>
+        <div className={styles.floating_value}>
+          {numberWithCommas(ambInfo.circulating_supply)} AMB
+        </div>
       </div>
 
       <div className={`${styles.floating_container} ${styles.total_supply}`}>
         <div className={styles.floating_name}>Total Supply</div>
-        <div className={styles.floating_value}>1,529,576,370</div>
+        <div className={styles.floating_value}>
+          {numberWithCommas(ambInfo.total_supply)} AMB
+        </div>
       </div>
 
       <div className={`${styles.floating_container} ${styles.market_cap}`}>
         <div className={styles.floating_name}>Market Cap</div>
-        <div className={styles.floating_value}>$19,836,144</div>
+        <div className={styles.floating_value}>
+          ${numberWithCommas(ambInfo.market_cap_usd)}
+        </div>
       </div>
     </div>
   );
