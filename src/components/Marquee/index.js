24b8cc745ea59022bab3b39e2794e2a57c5bb75d
d.voronin <echelon1109@gmail.com>
2023-10-12 15:36:29 +0300

    bugfix

--- src/components/Marquee/index.js
+++ src/components/Marquee/index.js
@@ -1,32 +1,45 @@
 import styles from './marquee.module.scss';
 import Image from 'next/image';
 import marqueeImg from './marquee-img.svg';
+import {useEffect, useState} from 'react';
 
-const Marquee = () => (
-  <div className={styles['marquee-wrapper']}>
-    <div className={styles['marquee-block-wrapper']}>
-      <div className={styles['marquee-block']}>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>SECURE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>NIMBLE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>TRANSPARENT</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>SECURE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>NIMBLE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>TRANSPARENT</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>SECURE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>NIMBLE</span>
-        <Image src={marqueeImg} alt="marquee image" />
-        <span className={styles['marquee-block__text']}>TRANSPARENT</span>
+const Marquee = () => {
+  const [isWindows, setIsWindows] = useState(false);
+
+  useEffect(() => {
+    if(!("navigator" in window)){
+      return "unknown";
+    }
+    const platform = (navigator.userAgentData?.platform || navigator.platform)?.toLowerCase();
+    setIsWindows(platform.startsWith('win'));
+  }, []);
+
+  return (
+    <div className={styles['marquee-wrapper']}>
+      <div className={styles['marquee-block-wrapper']}>
+        <div className={`${styles['marquee-block']} ${styles['marquee-block_win']}`}>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>SECURE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>NIMBLE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>TRANSPARENT</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>SECURE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>NIMBLE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>TRANSPARENT</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>SECURE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>NIMBLE</span>
+          <Image src={marqueeImg} alt="marquee image" />
+          <span className={styles['marquee-block__text']}>TRANSPARENT</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Marquee;
