d8a02a32a5f3d08e746daad91a61298b15dfdf24
d.voronin <echelon1109@gmail.com>
2023-10-18 15:36:52 +0300

    bond fix

--- src/pages/bond-marketplace/index.js
+++ src/pages/bond-marketplace/index.js
@@ -1,35 +1,31 @@
-'use client';
-import { Web3ReactProvider } from '@web3-react/core';
-import {
-  metamaskConnector,
-  metamaskHooks,
-  walletconnectConnector,
-  walletconnectHooks,
-} from 'airdao-components-and-tools/utils';
-import BondExchange from '@/components/Bond/BondExchange';
-import {createClient} from '@/prismicio';
-
-const connectors = [
-  [metamaskConnector, metamaskHooks],
-  [walletconnectConnector, walletconnectHooks],
-];
+// 'use client';
+// import { Web3ReactProvider } from '@web3-react/core';
+// import {
+//   metamaskConnector,
+//   metamaskHooks,
+//   walletconnectConnector,
+//   walletconnectHooks,
+// } from 'airdao-components-and-tools/utils';
+// import BondExchange from '@/components/Bond/BondExchange';
+// import {createClient} from '@/prismicio';
+//
+// const connectors = [
+//   [metamaskConnector, metamaskHooks],
+//   [walletconnectConnector, walletconnectHooks],
+// ];
 
 const HeaderWrapper = (props) => {
-  return (
-    <Web3ReactProvider connectors={connectors}>
-      <BondExchange {...props} />
-    </Web3ReactProvider>
-  );
+  return <div>asd</div>;
 };
-
-export async function getStaticProps({ params, previewData }) {
-  const client = createClient({ previewData });
-
-  const header = await client.getSingle('header');
-  const footer = await client.getSingle('footer');
-  return {
-    props: { header, footerText: footer },
-  };
-}
+//
+// export async function getStaticProps({ params, previewData }) {
+//   const client = createClient({ previewData });
+//
+//   const header = await client.getSingle('header');
+//   const footer = await client.getSingle('footer');
+//   return {
+//     props: { header, footerText: footer },
+//   };
+// }
 
 export default HeaderWrapper;
