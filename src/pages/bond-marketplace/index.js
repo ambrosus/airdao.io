5d808d6e4c2b4ba6f0220597cf54012c79102b26
d.voronin <echelon1109@gmail.com>
2023-10-18 15:03:33 +0300

    bond fix

--- src/pages/bond-marketplace/index.js
+++ src/pages/bond-marketplace/index.js
@@ -6,7 +6,7 @@ import {
   walletconnectConnector,
   walletconnectHooks,
 } from 'airdao-components-and-tools/utils';
-import BondExchange from '@/components/Bond//BondExchange';
+import BondExchange from '@/components/Bond/BondExchange';
 import {createClient} from '@/prismicio';
 
 const connectors = [
