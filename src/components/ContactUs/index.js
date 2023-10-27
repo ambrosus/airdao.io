78a934d20c8c57369878197ff93063d0a5f5078b
d.voronin <echelon1109@gmail.com>
2023-10-27 09:59:55 +0300

    contact us fix

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -2,10 +2,9 @@ import styles from './contact-us.module.scss';
 import Button from '@/components/Button';
 import Textarea from '@/components/Textarea';
 import Select from '@/components/Select';
-import React, { useState } from 'react';
-import { Notify, Input } from '@airdao/ui-library';
-import Head from 'next/head';
-import Link from 'next/link';
+import { useState } from 'react';
+import Input from '@/components/Input';
+import { Notify } from '@airdao/ui-library';
 
 export default function ContactUs() {
   const [formData, setFormData] = useState({
@@ -44,56 +43,44 @@ export default function ContactUs() {
   };
 
   return (
-    <section className="container">
-      <Head>
-        <meta property="og:image" content="https://airdao.io/og-contact.png" />
-        <meta name="twitter:image" content="https://airdao.io/og-contact.png" />
-      </Head>
-      <h1 className={styles.title}>
-        Share ideas, join the conversation, and learn about our ecosystem
-      </h1>
-      <div className={styles.links}>
-        <div>
-          <Link href="/">
-            Ideas
-          </Link>
-          <p>Share your ambitious ideas on how to take AirDAO to the next level.</p>
-        </div>
-      </div>
-      {/*<form onSubmit={handleSubmit} className={styles.form_container}>*/}
-      {/*  <Input*/}
-      {/*    placeholder={'Name'}*/}
-      {/*    required*/}
-      {/*    value={formData.name}*/}
-      {/*    onChange={(e) => setField('name', e.target.value)}*/}
-      {/*  />*/}
-      {/*  <Input*/}
-      {/*    placeholder={'Email'}*/}
-      {/*    required*/}
-      {/*    type="email"*/}
-      {/*    value={formData.email}*/}
-      {/*    onChange={(e) => setField('email', e.target.value)}*/}
-      {/*  />*/}
-      {/*  <Select*/}
-      {/*    placeholder="Select option"*/}
-      {/*    options={[*/}
-      {/*      'Tech support',*/}
-      {/*      'Business development',*/}
-      {/*      'Marketing and Press',*/}
-      {/*      'Other',*/}
-      {/*    ]}*/}
-      {/*    onChange={(value) => setField('category', value)}*/}
-      {/*    value={formData.category}*/}
-      {/*  />*/}
-      {/*  <Textarea*/}
-      {/*    placeholder={'Your text'}*/}
-      {/*    value={formData.message}*/}
-      {/*    onChange={(e) => setField('email', e.target.value)}*/}
-      {/*  />*/}
-      {/*  <Button type="primary" size="large">*/}
-      {/*    Confirm*/}
-      {/*  </Button>*/}
-      {/*</form>*/}
+    <section className={styles.contact_us}>
+      <h1 className={styles.heading}>Contact Us</h1>
+      <p className={styles.lead_text}>
+        The future is ours to build together. If you have ideas or opportunities
+        that <br />
+        you’d like to share with us, don’t hesitate to reach out today.
+      </p>
+      <form onSubmit={handleSubmit} className={styles.form_container}>
+        <Input
+          placeholder={'Name'}
+          required
+          onChange={(e) => setField('name', e.target.value)}
+        />
+        <Input
+          placeholder={'Email'}
+          required
+          type="email"
+          onChange={(e) => setField('email', e.target.value)}
+        />
+        <Select
+          placeholder="Select option"
+          options={[
+            'Tech support',
+            'Business development',
+            'Marketing and Press',
+            'Other',
+          ]}
+          onChange={(value) => setField('category', value)}
+          value={formData.category}
+        />
+        <Textarea
+          placeholder={'Your text'}
+          onChange={(e) => setField('email', e.target.value)}
+        />
+        <Button type="primary" size="large">
+          Confirm
+        </Button>
+      </form>
     </section>
   );
 }
