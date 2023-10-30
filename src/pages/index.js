ed97606058bf5c74cbaad914bffd5ba8bb991151
d.voronin <echelon1109@gmail.com>
2023-10-30 16:37:54 +0200

    perfomance test

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,129 +29,130 @@ export default function Home({ page, header, footerText, latestArticles }) {
 
   return (
     <div className={styles['homepage']}>
-      <Head>
-        <meta property="og:image" content="https://airdao.io/og.png" />
-        <meta name="twitter:image" content="https://airdao.io/og.png" />
-      </Head>
-      <HeaderWrapper header={header} />
-      <div className={styles['main-block-wrapper']}>
-        <Image
-          className={styles['blue-circle']}
-          src={blueCircle}
-          alt="blue circle"
-        />
-        <Image
-          className={styles['orange-circle']}
-          src={orangeCircle}
-          alt="orange circle"
-        />
-        <Image className={styles.shape} src={shape} alt="shape" />
-        <Image
-          className={`${styles.shape} ${styles['shape-right']}`}
-          src={shape}
-          alt="shape"
-        />
-        <MainBlock
-          title={data.title}
-          subtitle={data.subtitle}
-          label={data.label}
-          partners={data.partners}
-        />
-      </div>
-      <Community
-        text={data.text}
-        primaryLink={data.primary_btn_link}
-        primaryText={data.primary_btn_text}
-        secondaryLink={data.secondary_btn_link}
-        secondaryText={data.secondary_btn_text}
-      />
-      <Products title={data.product_title} products={data.products} />
-      <Mission
-        label={data.mission_label}
-        title={data.mission_title}
-        text={data.mission_text}
-      />
-      <Marquee />
-      <Team
-        title={data.team_title}
-        primaryText={data.team_primary_text}
-        primaryLink={data.team_primary_link}
-        image={data.team_image}
-      />
-      <Network
-        title={data.network_title}
-        label={data.network_label}
-        primaryText={data.network_primary_text}
-        primaryLink={data.network_primary_link}
-        info={data.network_info}
-      />
-      <div className={styles['ambassadors-wrapper']}>
-        <Image
-          className={styles['blue-circle']}
-          src={orangeCircle}
-          alt="blue circle"
-        />
-        <Image
-          className={styles['orange-circle']}
-          src={blueCircle}
-          alt="orange circle"
-        />
-        <Ambassadors
-          label={data.ambassadors_label}
-          title={data.ambassadors_title}
-          text={data.ambassadors_text}
-          images={data.ambassadors_images}
-          primaryText={data.ambassadors_primary_text}
-          primaryLink={data.ambassadors_primary_link}
-          secondaryText={data.ambassadors_secondary_text}
-          secondaryLink={data.ambassadors_secondary_link}
-        />
-      </div>
-      <Semiblocks
-        governanceTitle={data.governance_title}
-        governanceText={data.governance_text}
-        governanceLabel={data.governance_label}
-        governancePrimaryLink={data.governance_primary_link}
-        governancePrimaryText={data.governance_primary_text}
-        governanceSecondaryLink={data.governance_secondary_link}
-        governanceSecondaryText={data.governance_secondary_text}
-        communityTitle={data.community_title}
-        communityLabel={data.community_label}
-        communityText={data.community_text}
-        communityPrimaryText={data.community_primary_text}
-        communityPrimaryLink={data.community_primary_link}
-        communitySocials={data.community_socials}
-      />
-      <App
-        title={data.app_title}
-        list={data.app_list}
-        appstore={data.app_appstore}
-        google={data.app_google}
-      />
-      <div className={styles['articles-wrapper']}>
-        <Image
-          className={styles['blue-circle']}
-          src={blueCircle}
-          alt="blue circle"
-        />
-        <Image
-          className={styles['orange-circle']}
-          src={orangeCircle}
-          alt="orange circle"
-        />
-        {/*<ArticlesList*/}
-        {/*  title={asText(data.blog_title)}*/}
-        {/*  subtitle={asText(data.blog_subtitle)}*/}
-        {/*  goToText="Go to blog"*/}
-        {/*  goToLink="/"*/}
-        {/*  articles={latestArticles}*/}
-        {/*/>*/}
-      </div>
-      <Footer
-        slices={footerText.data.slices}
-        socials={footerText.data.footer_social}
-        footerBlock={footerSlice}
-      />
+      {/*<Head>*/}
+      {/*  <meta property="og:image" content="https://airdao.io/og.png" />*/}
+      {/*  <meta name="twitter:image" content="https://airdao.io/og.png" />*/}
+      {/*</Head>*/}
+      {/*<HeaderWrapper header={header} />*/}
+      {/*<div className={styles['main-block-wrapper']}>*/}
+      {/*  <Image*/}
+      {/*    className={styles['blue-circle']}*/}
+      {/*    src={blueCircle}*/}
+      {/*    alt="blue circle"*/}
+      {/*  />*/}
+      {/*  <Image*/}
+      {/*    className={styles['orange-circle']}*/}
+      {/*    src={orangeCircle}*/}
+      {/*    alt="orange circle"*/}
+      {/*  />*/}
+      {/*  <Image className={styles.shape} src={shape} alt="shape" />*/}
+      {/*  <Image*/}
+      {/*    className={`${styles.shape} ${styles['shape-right']}`}*/}
+      {/*    src={shape}*/}
+      {/*    alt="shape"*/}
+      {/*  />*/}
+      {/*  <MainBlock*/}
+      {/*    title={data.title}*/}
+      {/*    subtitle={data.subtitle}*/}
+      {/*    label={data.label}*/}
+      {/*    partners={data.partners}*/}
+      {/*  />*/}
+      {/*</div>*/}
+      {/*<Community*/}
+      {/*  text={data.text}*/}
+      {/*  primaryLink={data.primary_btn_link}*/}
+      {/*  primaryText={data.primary_btn_text}*/}
+      {/*  secondaryLink={data.secondary_btn_link}*/}
+      {/*  secondaryText={data.secondary_btn_text}*/}
+      {/*/>*/}
+      {/*<Products title={data.product_title} products={data.products} />*/}
+      {/*<Mission*/}
+      {/*  label={data.mission_label}*/}
+      {/*  title={data.mission_title}*/}
+      {/*  text={data.mission_text}*/}
+      {/*/>*/}
+      {/*<Marquee />*/}
+      {/*<Team*/}
+      {/*  title={data.team_title}*/}
+      {/*  primaryText={data.team_primary_text}*/}
+      {/*  primaryLink={data.team_primary_link}*/}
+      {/*  image={data.team_image}*/}
+      {/*/>*/}
+      {/*<Network*/}
+      {/*  title={data.network_title}*/}
+      {/*  label={data.network_label}*/}
+      {/*  primaryText={data.network_primary_text}*/}
+      {/*  primaryLink={data.network_primary_link}*/}
+      {/*  info={data.network_info}*/}
+      {/*/>*/}
+      {/*<div className={styles['ambassadors-wrapper']}>*/}
+      {/*  <Image*/}
+      {/*    className={styles['blue-circle']}*/}
+      {/*    src={orangeCircle}*/}
+      {/*    alt="blue circle"*/}
+      {/*  />*/}
+      {/*  <Image*/}
+      {/*    className={styles['orange-circle']}*/}
+      {/*    src={blueCircle}*/}
+      {/*    alt="orange circle"*/}
+      {/*  />*/}
+      {/*  <Ambassadors*/}
+      {/*    label={data.ambassadors_label}*/}
+      {/*    title={data.ambassadors_title}*/}
+      {/*    text={data.ambassadors_text}*/}
+      {/*    images={data.ambassadors_images}*/}
+      {/*    primaryText={data.ambassadors_primary_text}*/}
+      {/*    primaryLink={data.ambassadors_primary_link}*/}
+      {/*    secondaryText={data.ambassadors_secondary_text}*/}
+      {/*    secondaryLink={data.ambassadors_secondary_link}*/}
+      {/*  />*/}
+      {/*</div>*/}
+      {/*<Semiblocks*/}
+      {/*  governanceTitle={data.governance_title}*/}
+      {/*  governanceText={data.governance_text}*/}
+      {/*  governanceLabel={data.governance_label}*/}
+      {/*  governancePrimaryLink={data.governance_primary_link}*/}
+      {/*  governancePrimaryText={data.governance_primary_text}*/}
+      {/*  governanceSecondaryLink={data.governance_secondary_link}*/}
+      {/*  governanceSecondaryText={data.governance_secondary_text}*/}
+      {/*  communityTitle={data.community_title}*/}
+      {/*  communityLabel={data.community_label}*/}
+      {/*  communityText={data.community_text}*/}
+      {/*  communityPrimaryText={data.community_primary_text}*/}
+      {/*  communityPrimaryLink={data.community_primary_link}*/}
+      {/*  communitySocials={data.community_socials}*/}
+      {/*/>*/}
+      {/*<App*/}
+      {/*  title={data.app_title}*/}
+      {/*  list={data.app_list}*/}
+      {/*  appstore={data.app_appstore}*/}
+      {/*  google={data.app_google}*/}
+      {/*/>*/}
+      {/*<div className={styles['articles-wrapper']}>*/}
+      {/*  <Image*/}
+      {/*    className={styles['blue-circle']}*/}
+      {/*    src={blueCircle}*/}
+      {/*    alt="blue circle"*/}
+      {/*  />*/}
+      {/*  <Image*/}
+      {/*    className={styles['orange-circle']}*/}
+      {/*    src={orangeCircle}*/}
+      {/*    alt="orange circle"*/}
+      {/*  />*/}
+      {/*  /!*<ArticlesList*!/*/}
+      {/*  /!*  title={asText(data.blog_title)}*!/*/}
+      {/*  /!*  subtitle={asText(data.blog_subtitle)}*!/*/}
+      {/*  /!*  goToText="Go to blog"*!/*/}
+      {/*  /!*  goToLink="/"*!/*/}
+      {/*  /!*  articles={latestArticles}*!/*/}
+      {/*  /!*//*/}
+      {/*</div>*/}
+      {/*<Footer*/}
+      {/*  slices={footerText.data.slices}*/}
+      {/*  socials={footerText.data.footer_social}*/}
+      {/*  footerBlock={footerSlice}*/}
+      {/*/>*/}
+      <div>asdf</div>
     </div>
   );
 }
