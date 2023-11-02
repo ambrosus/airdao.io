d354eb6d1077e143c9bbf73ff9d2c8c72b2368f5
d.voronin <echelon1109@gmail.com>
2023-11-02 11:41:29 +0200

    prismic

--- src/pages/roadmap/index.js
+++ src/pages/roadmap/index.js
@@ -6,6 +6,10 @@ import {useMemo, useState} from 'react';
 import styles from './roadmap.module.scss';
 import Post from '@/pages/roadmap/components/Post';
 import {asText} from '@prismicio/client';
+import Slider from 'react-slick';
+import { Button } from '@airdao/ui-library';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const filters = [
   { labelKey: 'other_label', key: 'other' },
@@ -14,12 +18,77 @@ const filters = [
   { labelKey: 'multisig_label', key: 'multisig' },
 ];
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      className="slider-arrow"
+    >
+      <path
+        d="M8.40715 4.45305C8.01663 4.84357 8.01663 5.47674 8.40715 5.86726L14.7 12.1602L8.40715 18.453C8.01663 18.8436 8.01663 19.4767 8.40715 19.8673C8.79768 20.2578 9.43084 20.2578 9.82137 19.8673L16.8214 12.8673C17.2119 12.4767 17.2119 11.8436 16.8214 11.453L9.82136 4.45305C9.43084 4.06253 8.79768 4.06253 8.40715 4.45305Z"
+        fill="#212121"
+      />
+    </svg>
+  ),
+  prevArrow: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      className="slider-arrow"
+    >
+      <path
+        d="M8.40715 4.45305C8.01663 4.84357 8.01663 5.47674 8.40715 5.86726L14.7 12.1602L8.40715 18.453C8.01663 18.8436 8.01663 19.4767 8.40715 19.8673C8.79768 20.2578 9.43084 20.2578 9.82137 19.8673L16.8214 12.8673C17.2119 12.4767 17.2119 11.8436 16.8214 11.453L9.82136 4.45305C9.43084 4.06253 8.79768 4.06253 8.40715 4.45305Z"
+        fill="#212121"
+      />
+    </svg>
+  ),
+};
+
 const Roadmap = ({ header, footerText, page }) => {
   const [selectedFilter, setSelectedFilter] = useState('apollo');
+  console.log(page);
   return (
     <>
       {header && <HeaderWrapper header={header} />}
       <div className={`container ${styles.roadmap}`}>
+        <Slider {...settings}>
+          {page.slider.map((el) => (
+            <div key={asText(el.title)} className={styles.slider_item}>
+              <img src={el.image.url} alt="image" className={styles.slider_image} />
+              <div className={styles.slider_info}>
+                <PrismicRichText
+                  field={el.title}
+                  components={{
+                    paragraph: ({ children }) => (
+                      <h2 className={styles.slider_title}>{children}</h2>
+                    ),
+                  }}
+                />
+                <PrismicRichText
+                  field={el.description}
+                  components={{
+                    paragraph: ({ children }) => (
+                      <p className={styles.slider_description}>{children}</p>
+                    ),
+                  }}
+                />
+                <Button size="large" type="tetiary">Learn more</Button>
+              </div>
+            </div>
+          ))}
+        </Slider>
         <div className={styles.filters}>
           {filters.map((el) => (
             <PrismicRichText
@@ -53,7 +122,7 @@ const Roadmap = ({ header, footerText, page }) => {
         />
       )}
     </>
-  )
+  );
 };
 
 export async function getStaticProps({ params, previewData }) {
