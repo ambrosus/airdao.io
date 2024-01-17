baf55168816ca87bfeeb4b7390b72acc47aadd86
Vasyl Kovalchuk <kiewfenix@gmail.com>
2024-01-17 09:21:35 +0200

    add slider with test content

--- src/components/Team/TextBlock/index.js
+++ src/components/Team/TextBlock/index.js
@@ -1,27 +1,31 @@
-import styles from './text-block.module.scss';
 import BlockLabel from '@/components/BlockLabel';
+import TeamMediaSlider from '@/components/TeamMediaSlider';
 import { PrismicRichText } from '@prismicio/react';
-import { PrismicNextImage } from '@prismicio/next';
+import styles from './text-block.module.scss';
 
 export default function TextBlock({ story, illustration, mission, vision }) {
   return (
-    <div className={`container ${styles.container}`}>
-      <div className={styles.block}>
-        <BlockLabel className={`${styles.label} ${styles.label_first}`}>
-          OUR STORY
-        </BlockLabel>
-        <PrismicTextWrapper prismicText={story} />
+    <div className={`${styles.container}`}>
+      <div className="container">
+        <div className={styles.block}>
+          <BlockLabel className={`${styles.label} ${styles.label_first}`}>
+            OUR STORY
+          </BlockLabel>
+          <PrismicTextWrapper prismicText={story} />
+        </div>
       </div>
-
-      <PrismicNextImage field={illustration} className={styles.illustration} />
-
-      <div className={`${styles.block} ${styles.mission}`}>
-        <BlockLabel className={styles.label}>OUR MISSION</BlockLabel>
-        <PrismicTextWrapper prismicText={mission} />
+      <div className={styles.slider}>
+        <TeamMediaSlider />
       </div>
-      <div className={styles.block}>
-        <BlockLabel className={styles.label}>OUR VISION</BlockLabel>
-        <PrismicTextWrapper prismicText={vision} />
+      <div className="container">
+        <div className={`${styles.block} ${styles.mission}`}>
+          <BlockLabel className={styles.label}>OUR MISSION</BlockLabel>
+          <PrismicTextWrapper prismicText={mission} />
+        </div>
+        <div className={styles.block}>
+          <BlockLabel className={styles.label}>OUR VISION</BlockLabel>
+          <PrismicTextWrapper prismicText={vision} />
+        </div>
       </div>
     </div>
   );
