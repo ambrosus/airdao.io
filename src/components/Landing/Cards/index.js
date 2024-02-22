8059da3c22f5632c675701bc3f58349abcedc1a5
Shady <shady.assan@gmail.com>
2024-02-22 16:07:41 +0200

    remove image

--- src/components/Landing/Cards/index.js
+++ src/components/Landing/Cards/index.js
@@ -56,7 +56,7 @@ const Cards = () => {
       <Card
         id="2"
         title="Collect your first SBT to become AirDAO Governor"
-        icon={<TokenIcon />}
+        // icon={<TokenIcon />}
       >
         <p>
           The issuance of the AirDAO Governor SBT will be based on facial
@@ -66,7 +66,7 @@ const Cards = () => {
       <Card
         id="3"
         title="Let your role define your voting power"
-        icon={<RoleIcon />}
+        // icon={<RoleIcon />}
       >
         <p>
           Your engagement matters! The governance voting power will be based on
@@ -92,7 +92,7 @@ const Cards = () => {
       <Card
         id="5"
         title="Recognition of Diverse contributions"
-        icon={<CardsIcon />}
+        // icon={<CardsIcon />}
       >
         <p>
           Whether you serve as a Validator or OG, Ambassador or marketer,
