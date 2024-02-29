0ed41be82c7d82181272e4ed8954a8ac8741b3d1
Shady <shady.assan@gmail.com>
2024-02-29 16:18:03 +0200

    release 2.0, added get started buttons

--- src/components/Landing/BannerMap/index.js
+++ src/components/Landing/BannerMap/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import Link from 'next/link';
 import { Button } from '@airdao/ui-library';
 
 import styles from './banner.module.scss';
@@ -6,7 +6,6 @@ import styles from './banner.module.scss';
 import WorldRight from '@/components/Icons/WorldRight';
 import WorldLeft from '@/components/Icons/WorldLeft';
 import ChevronIcon from '@/components/Icons/ChevronIcon';
-import Link from 'next/link';
 
 const BannerMap = () => {
   return (
@@ -20,9 +19,14 @@ const BannerMap = () => {
           community member has voting rights and governance power according to
           their engagement via AirDAO Governor SBT
         </p>
-        {/* <Button size="large" type="tetiary">
-          Get started
-        </Button> */}
+        <Link
+          target="_blank"
+          href="https://airdao.io/gov-portal/connect-wallet"
+        >
+          <Button size="large" type="tetiary">
+            Get started
+          </Button>
+        </Link>
         <Link target="_blank" href="https://airdao.io/academy#governance">
           <Button size="large" type="primary">
             Learn more
