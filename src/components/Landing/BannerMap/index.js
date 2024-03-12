d605a01b96ed94117c33d3eeb8aa0a9b1139a4bd
Shady <shady.assan@gmail.com>
2024-03-12 23:17:27 +0200

    fix banner

--- src/components/Landing/BannerMap/index.js
+++ src/components/Landing/BannerMap/index.js
@@ -3,15 +3,15 @@ import { Button } from '@airdao/ui-library';
 
 import styles from './banner.module.scss';
 
-import WorldRight from '@/components/Icons/WorldRight';
-import WorldLeft from '@/components/Icons/WorldLeft';
+// import WorldRight from '@/components/Icons/WorldRight';
+// import WorldLeft from '@/components/Icons/WorldLeft';
 import ChevronIcon from '@/components/Icons/ChevronIcon';
 
 const BannerMap = () => {
   return (
     <div className={styles.banner}>
-      <WorldLeft className={styles.worldLeft} />
-      <WorldRight className={styles.worldRight} />
+      {/* <WorldLeft className={styles.worldLeft} />
+      <WorldRight className={styles.worldRight} /> */}
       <div className={styles.holder}>
         <h3>New era of AirDAO Governance</h3>
         <p>
