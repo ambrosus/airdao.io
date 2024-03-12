77e7d84a72dd83a38779e5ffe15413e8abc08311
Shady <shady.assan@gmail.com>
2024-03-12 13:26:57 +0200

    fix header

--- src/pages/gov-portal/index.js
+++ src/pages/gov-portal/index.js
@@ -30,7 +30,7 @@ export default function Landing({ page, header, banner, footerText }) {
           nextLink={false}
         />
       )}
-      <HeaderWrapper header={header} showBanner={true} />
+      <HeaderWrapper header={header} showBanner={showBanner} />
       <div className={styles.content}>
         <BannerMap />
         <div className={styles.insetRound}>
