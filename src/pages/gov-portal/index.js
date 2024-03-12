b03315d6db3c3ea22f1ff2a78d16f57cddfb5ea7
Shady <shady.assan@gmail.com>
2024-03-12 23:40:51 +0200

    fix cards

--- src/pages/gov-portal/index.js
+++ src/pages/gov-portal/index.js
@@ -38,12 +38,12 @@ export default function Landing({ page, header, banner, footerText }) {
             <h3 className={styles['content-title']}>
               Embark on your Governance journey in 3 steps
             </h3>
-            <Steps />
+            {/* <Steps /> */}
             <h2 className={styles['section-title']}>
               <span>Empower Your Influence:</span> Enhancing Decision-Making in
               AirDAO
             </h2>
-            {/* <Cards /> */}
+            <Cards />
           </div>
         </div>
         <div className={styles.cta}>
