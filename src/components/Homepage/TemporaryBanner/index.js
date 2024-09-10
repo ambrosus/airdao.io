7e20f24cf22ce50c97d2e4aba501156433b6f0c4
d.voronin <echelon1109@gmail.com>
2024-09-10 13:01:46 +0300

    add new banner

---
+++ src/components/Homepage/TemporaryBanner/index.js
@@ -0,0 +1,45 @@
+import styles from './temp-banner.module.scss';
+import sfIcon from './starfleet.svg';
+import Image from 'next/image';
+import chevron from '@/assets/icons/chevron-additional.svg';
+import { Button } from '@airdao/ui-library';
+import { useEffect, useState } from 'react';
+
+const targetDate = '2024-09-15';
+
+const TemporaryBanner = () => {
+  const [remainingDays, setRemainingDays] = useState(0);
+
+  useEffect(() => {
+    const calculateRemainingDays = () => {
+      const currentDate = new Date();
+      const target = new Date(targetDate);
+      const timeDiff = target - currentDate;
+      const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+      setRemainingDays(daysDiff);
+    };
+
+    calculateRemainingDays();
+  }, [targetDate]);
+
+  return (
+    <section className="container">
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <span className={styles.day}>{remainingDays}</span>
+          <span className={styles.day_text}>days left</span>
+        </div>
+        <div className={styles.right}>
+          <img src={sfIcon.src} alt="starfleet" />
+          <p className={styles.text}><span>Final warning!</span> Stakers must be aboard the ship</p>
+          <Button type="primary" size="large" className={styles.btn}>
+            Learn more
+            <Image src={chevron} alt="Continue Button" />
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default TemporaryBanner;
