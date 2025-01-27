67684c251fbcea32cad7ca4f08714551bce96ca9
Coguson <Coguson@gmail.com>
2025-01-27 10:10:05 +0200

    build error fix

--- src/pages/grants/components/Grants.js
+++ src/pages/grants/components/Grants.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from '../grants.module.scss';
-import { grantTiers } from './data';
 
 import Logo from './assets/logo.svg';
 import Img1 from './assets/group-photo.webp';
 import Img2 from './assets/journey-illustration.svg';
 import Img3 from './assets/challenge-illustration.svg';
+
+import Tier1 from './assets/tier-1.svg';
+import Tier2 from './assets/tier-2.svg';
+import Tier3 from './assets/tier-3.svg';
+import Tier4 from './assets/tier-4.svg';
+
+const grantTiers = {
+  card1: {
+    img: Tier1,
+    alt: 'early_stage_icon',
+    heading: 'Support early-stage ideas or prototypes',
+    description: 'Basic proof of concept or clear roadmap required.',
+  },
+  card2: {
+    img: Tier2,
+    alt: 'development_icon',
+    heading: 'Fund projects in the development phase that have shown potential',
+    description:
+      'Detailed project plan, technical specifications, and initial traction or community interest.',
+  },
+  card3: {
+    img: Tier3,
+    alt: 'large_scale_icon',
+    heading: 'Large-scale projects with significant impact potential',
+    description:
+      'Proven track record, detailed budget, and comprehensive milestones.',
+  },
+  card4: {
+    img: Tier4,
+    alt: 'game_changer_icon',
+    heading: 'Game changer',
+    description:
+      'Strategic alignment with AirDAO goals, co-funding, or resource-sharing agreements.',
+  },
+};
 
 const Grants = ({ email }) => {
   return (
